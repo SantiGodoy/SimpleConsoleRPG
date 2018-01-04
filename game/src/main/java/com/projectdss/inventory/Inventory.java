6a43ea9e8014ec41d9f39c476dd49096d875de86
JoseCorrero <jose_correro@hotmail.com>
2018-01-04 02:13:44 +0100

    Implemented class Inventory.

--- game/src/main/java/com/projectdss/inventory/Inventory.java
+++ game/src/main/java/com/projectdss/inventory/Inventory.java
@@ -1,16 +1,67 @@
+package com.projectdss.inventory;
 
-import java.util.*;
+import java.util.Set;
+import java.util.Iterator;
+import com.projectdss.Item;
+import com.projectdss.item.combatitem.equipablecombatitem.ArmorEquipableCombatItem;
+import com.projectdss.item.combatitem.equipablecombatitem.WeaponEquipableCombatItem;
 
 /**
+ * @author JoseCorrero
  * 
+ * Class Inventory.
  */
-public abstract class Inventory {
+public abstract class Inventory implements Iterable<Item> {
 
-    /**
-     * Default constructor
-     */
-    public Inventory() {
+    private Set<Item> items;
+    private WeaponEquipableCombatItem equippedWeapon;
+    private ArmorEquipableCombatItem equippedArmor;
+
+    public Inventory(Set<Item> items, WeaponEquipableCombatItem weapon, ArmorEquipableCombatItem armor) {
+        this.items = items;
+        equippedWeapon = weapon;
+        equippedArmor = armor;
     }
 
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    public void removeItem(Item item) {
+        items.remove(item);
+    }
+
+    public void setEquippedWeapon(WeaponEquipableCombatItem weapon) {
+        equippedWeapon = weapon;
+    }
+
+    public void setEquippedArmor(ArmorEquipableCombatItem armor) {
+        equippedArmor = armor;
+    }
+
+    public boolean hasWeaponEquipped() {
+        return equippedWeapon != null;
+    }
+
+    public boolean hasArmorEquipped() {
+        return equippedArmor != null;
+    }
+
+    public WeaponEquipableCombatItem getEquippedWeapon() {
+        return equippedWeapon;
+    }
+
+    public ArmorEquipableCombatItem getEquippedArmor() {
+        return equippedArmor;
+    }
+
+    public Set<Item> getItems() {
+        return items;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return items.iterator();
+    }
 
 }
