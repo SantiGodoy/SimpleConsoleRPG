d68b44ee2040ff557c5fd925d671db502a345150
SantiGodoy <godoy.programmer@gmail.com>
2018-01-21 22:30:19 +0100

    Updated Saver

--- game/src/main/java/com/projectdss/Saver.java
+++ game/src/main/java/com/projectdss/Saver.java
@@ -2,7 +2,10 @@ package com.projectdss;
 
 import com.projectdss.character.MainCharacter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.channels.FileChannel;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -20,22 +23,51 @@ import org.xml.sax.SAXException;
 
 public class Saver {
 
+    private void copyFile(File sourceFile, File destFile) throws IOException {
+        if(!destFile.exists()) {
+            destFile.createNewFile();
+        }
+    
+        FileChannel source = null;
+        FileChannel destination = null;
+    
+        try {
+            source = new FileInputStream(sourceFile).getChannel();
+            destination = new FileOutputStream(destFile).getChannel();
+            destination.transferFrom(source, 0, source.size());
+        }
+        finally {
+            if(source != null) {
+                source.close();
+            }
+            if(destination != null) {
+                destination.close();
+            }
+        }
+    }
+
     public MainCharacter saveMainCharacter(String filePath) {
         try {
+
+            File loadedFile = new File("../src/main/resources/map.xml");
+            File savedFile = new File("../src/main/resources/save.xml");
+
+            copyFile(loadedFile, savedFile);
+
             DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
             Document doc = docBuilder.parse(filePath);
 
+            Node minizone00 = doc.getElementsByTagName("bean").item(0);
+            Node attribute = minizone00.getAttributes().getNamedItem("name");
+
             //write the content into xml file
-            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-		    Transformer transformer = transformerFactory.newTransformer();
-		    DOMSource source = new DOMSource(doc);
-		    StreamResult result = new StreamResult(new File(filePath));
-		    transformer.transform(source, result);
+            Transformer xformer = TransformerFactory.newInstance().newTransformer();
+            xformer.transform(new DOMSource(doc), new StreamResult(new File(filePath)));
 
-		    System.out.println("Done");
+		    System.out.println("Game saved");
 
-        } catch (ParserConfigurationException pce) {
+           } catch (ParserConfigurationException pce) {
             pce.printStackTrace();
            } catch (TransformerException tfe) {
             tfe.printStackTrace();
